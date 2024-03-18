04625f073969d69f7a0a2e2fcb0f433460a23233
Abdul Rahman Azam <69141440+AbdulRahmanAzam@users.noreply.github.com>
2024-03-18 11:57:00 +0500

    Create 36.ts

---
+++ Typescript/Assignment1/36.ts
@@ -0,0 +1,7 @@
+function make_shirt(size, text){
+    console.log(`The size of the shirt is ${size} and the text written over it is ${text}`);
+}
+
+let size:  number = 5;
+let text: string = "WOW"
+make_shirt(size, text);
